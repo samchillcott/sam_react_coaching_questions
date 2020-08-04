b29533b47887f7fa02f7a40184119a343a25ac45
Sam Chillcott <sam@samchillcott.com>
2020-08-04 13:46:51 +0100

    string check

--- src/utils.test.js
+++ src/utils.test.js
@@ -7,16 +7,19 @@ const { toBeOneOf } = require("./setupTests");
 test("should select an item from an array at random", () => {
 	expect(selectRandomItemFromArray([1, 2])).toBeOneOf([1, 2]);
 	expect(selectRandomItemFromArray([43, 44, 45])).toBeOneOf([43, 44, 45]);
-	// expect(selectRandomItemFromArray([])).toBe(undefined);
 	expect(() => {
-        selectRandomItemFromArray(null).toThrow(TypeError);
-    });
+		selectRandomItemFromArray(null).toThrow(TypeError);
+	});
 	expect(() => {
-        selectRandomItemFromArray([]).toThrow(TypeError);
-    });
-	// expect(selectRandomItemFromArray("I am a string not an array")).toThrow(TypeError);
+		selectRandomItemFromArray([]).toThrow(TypeError);
+	});
+	expect(() => {
+		selectRandomItemFromArray("I am a string not an Array").toThrow(TypeError);
+	});
 });
 
-// Integration Test
+// Integration Tests
+
+
 
 // e2e Test
