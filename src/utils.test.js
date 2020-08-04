c58354f00db1bac51f9ea2bb49dc852d7a7e9e3d
Sam Chillcott <sam@samchillcott.com>
2020-08-04 12:29:35 +0100

    imported jest extend + unit test working tobooneof

--- src/utils.test.js
+++ src/utils.test.js
@@ -1,13 +1,18 @@
 const { selectRandomItemFromArray } = require("./utils");
 const puppeteer = require("puppeteer");
+// import '@testing-library/jest-dom';
+const { toBeOneOf } = require('./setupTests')
+
 
 // Unit Test
 
 test("should select an item from an array at random", () => {
 	;
-	expect(selectRandomItemFromArray([1, 2])).toBeOneOf([1, 2]);
+	// expect(selectRandomItemFromArray([1, 2])).toBeOneOf([1, 2]);
+	expect(selectRandomItemFromArray([43, 44, 45])).toBeOneOf([43, 44, 45]);
 	// const selection = selectRandomItemFromArray([1, 2, 3, 4]);
-	// expect(selection).toBe(1 || 2 || 3 || 4);
+	// expect(selection).toMatch(/^1|2|3|4$/);
+	// expect(selection).toBeOneOf([1, 2, 3, 4]);
 });
 
 // Integration Test
