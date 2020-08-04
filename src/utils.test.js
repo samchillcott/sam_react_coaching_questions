40d0667513a1f37ca9dc4ea8fe3db0074c0ced31
Sam Chillcott <sam@samchillcott.com>
2020-08-04 10:08:55 +0100

    unit test draft

--- src/utils.test.js
+++ src/utils.test.js
@@ -1,2 +1,13 @@
 const { selectRandomItemFromArray } = require("./utils");
 const puppeteer = require("puppeteer");
+
+// Unit Test
+
+test("should select an item from an array at random", () => {
+	const selection = selectRandomItemFromArray([1, 2, 3, 4]);
+	expect(selection).toBe(1 || 2 || 3 || 4);
+});
+
+// Integration Test
+
+// e2e Test
