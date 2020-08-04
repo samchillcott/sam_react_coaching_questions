e511569ed708428dc16e9cc188b897287871c19c
Sam Chillcott <sam@samchillcott.com>
2020-08-04 13:20:27 +0100

    unit test exceptions

--- src/utils.test.js
+++ src/utils.test.js
@@ -2,12 +2,18 @@ const { selectRandomItemFromArray } = require("./utils");
 const puppeteer = require("puppeteer");
 const { toBeOneOf } = require("./setupTests");
 
-// Unit Test
+// Unit Tests
 
 test("should select an item from an array at random", () => {
 	expect(selectRandomItemFromArray([1, 2])).toBeOneOf([1, 2]);
 	expect(selectRandomItemFromArray([43, 44, 45])).toBeOneOf([43, 44, 45]);
-	expect(selectRandomItemFromArray([])).toBe(undefined);
+	// expect(selectRandomItemFromArray([])).toBe(undefined);
+	expect(() => {
+        selectRandomItemFromArray(null).toThrow(TypeError);
+    });
+	expect(() => {
+        selectRandomItemFromArray([]).toThrow(TypeError);
+    });
 	// expect(selectRandomItemFromArray("I am a string not an array")).toThrow(TypeError);
 });
 
