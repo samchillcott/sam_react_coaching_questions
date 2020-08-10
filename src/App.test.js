1d8ea3c567ee7bfc431b41f958b159d5c3b25938
Sam Chillcott <sam@samchillcott.com>
2020-08-10 08:31:33 +0100

    app.test tidy

--- src/App.test.js
+++ src/App.test.js
@@ -1,21 +1,21 @@
-// Integration Tests
 const { selectRandomItemFromArray } = require("./utils");
 const puppeteer = require("puppeteer");
 const { toBeOneOf } = require("./setupTests");
 const { questions } = require("./questions");
 
+// Integration Tests
 
 describe("randomize function", () => {
 	it("should select an item from the data (copy of questions) array at random", () => {
-        // let arr1 = questions;
-        let data = [...questions];
+		// let arr1 = questions;
+		let data = [...questions];
 		let choice = selectRandomItemFromArray(data);
-		// expect(choice).toBeOneOf(questions);
 		expect(choice).toBeOneOf(questions);
 	});
 });
 
 // e2e Test
+
 describe("e2e Test", () => {
 	test("should display random question inside question component when button clicked", async () => {
 		const browser = await puppeteer.launch({
@@ -24,10 +24,10 @@ describe("e2e Test", () => {
 			args: ["--window-size=1000,800"],
 		});
 		const page = await browser.newPage();
-		await page.goto('http://localhost:3000/');
+		await page.goto("http://localhost:3000/");
 
 		await page.click("button");
-	
+
 		let question = await page.$eval(".question", (el) => el.textContent);
 		expect(question).toBeOneOf(questions);
 	}, 300000);
