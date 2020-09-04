19f3cc7046b4a9e33f69b9b734c40346ea285df0
Sam Chillcott <sam@samchillcott.com>
2020-09-04 14:48:50 +0100

    docblock

--- src/utils.js
+++ src/utils.js
@@ -1,14 +1,19 @@
+/**
+ * Function selects an item from array at random. Will throw if array is empty or argument is not an array.
+ * @param  {object} arr
+ * @throws {object}
+ */
 const selectRandomItemFromArray = (arr) => {
 	if (arr.constructor !== Array) {
 		throw new Error("Input is not an array");
 	} else if (arr.length === 0) {
 		throw new Error("Array is empty");
-	// } else if (
-	// 	arr.every(function (element) {
-	// 		return typeof element !== "number";
-	// 	})
-	// ) {
-	// 	throw new Error("Array contains a non-integer");
+		// } else if (
+		// 	arr.every(function (element) {
+		// 		return typeof element !== "number";
+		// 	})
+		// ) {
+		// 	throw new Error("Array contains a non-integer");
 	} else {
 		const index = Math.floor(Math.random() * arr.length);
 		let choice = arr.splice(index, 1)[0];
